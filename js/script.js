d0e5b5f2a73b182deebf63eae8f040572cb83161
8devil3 <8dev.il3@gmail.com>
2022-02-23 15:12:48 +0100

    corretto conteggio che va da 1 a 100, non da zero

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ let numberBox;
 
 
 // Mauro, lo so che detesti i contatori nominati con "i", "x", ecc. ma per ora va bene così
-for (x = 0; x <= 100; x++) {
+for (x = 1; x <= 100; x++) {
     
     numberBox = document.createElement('div');
     numberBox.classList.add('number-box');
 
-    if (x % 3 == 0   &&   x != 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
+    if (x % 3 == 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
 
         numberBox.classList.add('fizz');
         numberBox.innerHTML = "Fizz";
         
-    } else if (x % 5 == 0   &&   x != 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
+    } else if (x % 5 == 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
         
         numberBox.classList.add('buzz');
         numberBox.innerHTML = "Buzz";
         
-    } else if (x % 3 == 0   &&   x % 5 == 0   &&   x != 0) {
+    } else if (x % 3 == 0   &&   x % 5 == 0) {
         
         numberBox.classList.add('fizz-buzz');
         numberBox.innerHTML = "FizzBuzz";
