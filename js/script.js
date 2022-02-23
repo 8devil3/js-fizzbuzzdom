80accc8485b4a4ce5a0bed68215f13a4c134cd31
8devil3 <8dev.il3@gmail.com>
2022-02-23 15:10:25 +0100

    aggiunti spazi nelle condizioni

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ for (x = 0; x <= 100; x++) {
     numberBox = document.createElement('div');
     numberBox.classList.add('number-box');
 
-    if (x % 3 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
+    if (x % 3 == 0   &&   x != 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
 
         numberBox.classList.add('fizz');
         numberBox.innerHTML = "Fizz";
         
-    } else if (x % 5 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
+    } else if (x % 5 == 0   &&   x != 0   &&   !(x % 3 == 0   &&   x % 5 == 0)) {
         
         numberBox.classList.add('buzz');
         numberBox.innerHTML = "Buzz";
         
-    } else if (x % 3 == 0 && x % 5 == 0 && x != 0) {
+    } else if (x % 3 == 0   &&   x % 5 == 0   &&   x != 0) {
         
         numberBox.classList.add('fizz-buzz');
         numberBox.innerHTML = "FizzBuzz";
