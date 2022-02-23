3c09dd7472641d255e3706515876eeafc87e1f18
8devil3 <8dev.il3@gmail.com>
2022-02-23 14:46:26 +0100

    aggiunto stile

--- js/script.js
+++ js/script.js
@@ -3,27 +3,31 @@ let numberBox;
 
 
 
-for (x = 0; x <= 20; x++) {
+for (x = 0; x <= 100; x++) {
     
     if (x % 3 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
         
         numberBox = document.createElement('div');
+        numberBox.classList.add('number-box', 'fizz');
         numberBox.innerHTML = "Fizz";
         
     } else if (x % 5 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
         
         numberBox = document.createElement('div');
+        numberBox.classList.add('number-box', 'buzz');
         numberBox.innerHTML = "Buzz";
         
     } else if (x % 3 == 0 && x % 5 == 0 && x != 0) {
         
         numberBox = document.createElement('div');
+        numberBox.classList.add('number-box', 'fizz-buzz');
         numberBox.innerHTML = "FizzBuzz";
         
     }
     else {
 
         numberBox = document.createElement('div');
+        numberBox.classList.add('number-box');
         numberBox.innerHTML = x;
 
     }
