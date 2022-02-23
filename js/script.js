0c874a7d29e326dc93e5b4cf08cb4fd061b90275
8devil3 <8dev.il3@gmail.com>
2022-02-23 14:33:00 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+let divContainer = document.querySelector('.container');
+let numberBox;
+
+
 
 for (x = 0; x <= 20; x++) {
-
-    if ((x % 3 == 0 && x != 0) && !(x % 3 == 0 && x % 5 == 0)) {
-
-        console.log("Fizz");
-
-    } else if ((x % 5 == 0 && x != 0) && !(x % 3 == 0 && x % 5 == 0)) {
-
-        console.log("Buzz");
-
+    
+    if (x % 3 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
+        
+        numberBox = document.createElement('div');
+        numberBox.innerHTML = "Fizz";
+        
+    } else if (x % 5 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
+        
+        numberBox = document.createElement('div');
+        numberBox.innerHTML = "Buzz";
+        
     } else if (x % 3 == 0 && x % 5 == 0 && x != 0) {
-
-        console.log("FizzBuzz");
-
+        
+        numberBox = document.createElement('div');
+        numberBox.innerHTML = "FizzBuzz";
+        
     }
     else {
-        console.log(x);
+
+        numberBox = document.createElement('div');
+        numberBox.innerHTML = x;
+
     }
+    
+    divContainer.append(numberBox);
 }
