f72ba838e18c600b99f45afd582444aef69dc935
8devil3 <8dev.il3@gmail.com>
2022-02-23 14:57:05 +0100

    corretto js

--- js/script.js
+++ js/script.js
@@ -5,28 +5,26 @@ let numberBox;
 
 for (x = 0; x <= 100; x++) {
     
+    numberBox = document.createElement('div');
+
     if (x % 3 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
-        
-        numberBox = document.createElement('div');
+
         numberBox.classList.add('number-box', 'fizz');
         numberBox.innerHTML = "Fizz";
         
     } else if (x % 5 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
         
-        numberBox = document.createElement('div');
         numberBox.classList.add('number-box', 'buzz');
         numberBox.innerHTML = "Buzz";
         
     } else if (x % 3 == 0 && x % 5 == 0 && x != 0) {
         
-        numberBox = document.createElement('div');
         numberBox.classList.add('number-box', 'fizz-buzz');
         numberBox.innerHTML = "FizzBuzz";
         
     }
     else {
 
-        numberBox = document.createElement('div');
         numberBox.classList.add('number-box');
         numberBox.innerHTML = x;
 
