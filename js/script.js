f18e7341e639ea1d30fa2dd364d0d416c84ae6fb
8devil3 <8dev.il3@gmail.com>
2022-02-23 15:09:07 +0100

    aggiunto commento al js

--- js/script.js
+++ js/script.js
@@ -2,30 +2,29 @@ let divContainer = document.querySelector('.container');
 let numberBox;
 
 
-
+// Mauro, lo so che detesti i contatori nominati con "i", "x", ecc. ma per ora va bene così
 for (x = 0; x <= 100; x++) {
     
     numberBox = document.createElement('div');
+    numberBox.classList.add('number-box');
 
     if (x % 3 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
 
-        numberBox.classList.add('number-box', 'fizz');
+        numberBox.classList.add('fizz');
         numberBox.innerHTML = "Fizz";
         
     } else if (x % 5 == 0 && x != 0 && !(x % 3 == 0 && x % 5 == 0)) {
         
-        numberBox.classList.add('number-box', 'buzz');
+        numberBox.classList.add('buzz');
         numberBox.innerHTML = "Buzz";
         
     } else if (x % 3 == 0 && x % 5 == 0 && x != 0) {
         
-        numberBox.classList.add('number-box', 'fizz-buzz');
+        numberBox.classList.add('fizz-buzz');
         numberBox.innerHTML = "FizzBuzz";
         
-    }
-    else {
+    } else {
 
-        numberBox.classList.add('number-box');
         numberBox.innerHTML = x;
 
     }
